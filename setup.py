efeea3518ba04f9b3ba9baa87d0ae6b7a46167c6
Midnighter <midnighter@posteo.net>
2020-03-02 03:23:05 +0100

    style: reformat header

--- setup.py
+++ setup.py
@@ -1,29 +1,29 @@
-## @file    setup.py
-## @brief   Python distutils code for libSBML Python module (including dependencies)
-## @author  Michael Hucka
-## @author  Ben Bornstein
-## @author  Ben Kovitz
-## @author  Frank Bergmann (fbergman@caltech.edu)
-##
-##<!---------------------------------------------------------------------------
-## This file is part of libSBML.  Please visit http://sbml.org for more
-## information about SBML, and the latest version of libSBML.
-##
-## Copyright (C) 2013-2018 jointly by the following organizations:
-##     1. California Institute of Technology, Pasadena, CA, USA
-##     2. EMBL European Bioinformatics Institute (EMBL-EBI), Hinxton, UK
-##     3. University of Heidelberg, Heidelberg, Germany
-##
-## Copyright 2005-2010 California Institute of Technology.
-## Copyright 2002-2005 California Institute of Technology and
-##                     Japan Science and Technology Corporation.
-##
-## This library is free software; you can redistribute it and/or modify it
-## under the terms of the GNU Lesser General Public License as published by
-## the Free Software Foundation.  A copy of the license agreement is provided
-## in the file named "LICENSE.txt" included with this software distribution
-## and also available online as http://sbml.org/software/libsbml/license.html
-##----------------------------------------------------------------------- -->*/
+# @file    setup.py
+# @brief   Python distutils code for libSBML Python module (including dependencies)
+# @author  Michael Hucka
+# @author  Ben Bornstein
+# @author  Ben Kovitz
+# @author  Frank Bergmann (fbergman@caltech.edu)
+#
+#
+# This file is part of libSBML.  Please visit http://sbml.org for more
+# information about SBML, and the latest version of libSBML.
+#
+# Copyright (C) 2013-2018 jointly by the following organizations:
+#     1. California Institute of Technology, Pasadena, CA, USA
+#     2. EMBL European Bioinformatics Institute (EMBL-EBI), Hinxton, UK
+#     3. University of Heidelberg, Heidelberg, Germany
+#
+# Copyright 2005-2010 California Institute of Technology.
+# Copyright 2002-2005 California Institute of Technology and
+#                     Japan Science and Technology Corporation.
+#
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation.  A copy of the license agreement is provided
+# in the file named "LICENSE.txt" included with this software distribution
+# and also available online as http://sbml.org/software/libsbml/license.html
+
 
 import glob
 import os
